88a089275c7b110488bf353025a80f930b9ef888
arishafareed <arishafareed175@gmail.com>
2024-12-27 15:59:34 +0500

    arrays-method

---
+++ script.js
@@ -0,0 +1,48 @@
+// Arrays Method
+let foodItems = ["Samsung", "Galaxy", "Vivo", "Infinix"];
+console.log(foodItems);
+//fooditems.push("Samsung", "Galaxy", "Vivo", "Infinix")
+foodItems.push();
+foodItems.pop();
+
+let color = ["yellow","greeen","blue","red"];
+let newcolor = color.toString();
+console.log(newcolor);
+
+let num = [ 2398, 345, 385, 3945];
+let  = newnum =num.toString();
+console.log(newnum);
+ 
+
+
+let fooditem1 = ["apple","mango","peach", "banana"];
+let color1 = ["yellow","greeen","blue","red"];
+let num1 = [ 2398, 345, 385, 3945];
+
+let newitem = foodItems.concat(color1,num1);
+console.log(newitem);
+
+let company = ["Samsung", "Galaxy", "Vivo", "Infinix"];
+company.shift();
+console.log(company);
+
+let browser = ["apple","mango","peach", "banana"]
+browser.unshift();
+console.log(browser);0
+
+let vegetable = ["potato", "lemon", "cocumber", "chili"];
+let fruit = vegetable.slice(-3, -1);
+console.log(vegetable);
+vegetable.splice(2,0, "tomato","kivi");
+
+
+let companies = ["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+console.log(companies);
+companies.shift();
+console.log(companies);
+companies.splice(1,1, "Ola");
+companies.push("Amazon");
+console.log(companies);
+
+
+
